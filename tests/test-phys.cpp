81337fbc8e03c1dba6a72ac71f913247f3a3949e
Juho Peltonen <juhopel@gmail.com>
2014-11-01 19:46:08 +0200

    fix test

--- tests/test-phys.cpp
+++ tests/test-phys.cpp
@@ -10,11 +10,11 @@ int main() {
 
     physics::World phys;
 
-    phys.add_cube(0, trans, 1.0);
-    phys.add_cube(1, trans2, 1.0);
+    phys.add_cube(0, trans, 1.0, 1, 1, 1);
+    phys.add_cube(1, trans2, 1.0, 1, 1, 1);
 
-    glm::mat4 groundtrans = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -10.0f, -30.0f)), glm::vec3(50,1,50));
-    phys.add_cube(2, groundtrans, 0.0);
+    glm::mat4 groundtrans = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -10.0f, -30.0f));
+    phys.add_cube(2, groundtrans, 0.0, 50, 1, 50);
 
     for (int i = 0; i < 5; i++) {
         phys.single_step();
