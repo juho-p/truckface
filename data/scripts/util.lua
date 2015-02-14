725836323a9180b0a2ccce4786d4126bb6adf9c7
Juho Peltonen <juhopel@gmail.com>
2015-02-14 15:51:35 +0200

    some lua functions

--- data/scripts/util.lua
+++ data/scripts/util.lua
@@ -1,7 +1,29 @@
+-- map array arr with function fn
 function map(fn, arr)
     local new_arr = {}
     for i,v in ipairs(arr) do
         new_arr[i] = fn(v)
     end
     return new_arr
+end
+
+-- filter array arr using function fn
+function filter(fn, arr)
+    local new_arr = {}
+    j = 1
+    for i,v in ipairs(arr) do
+        if fn(v) then
+            new_arr[j] = v
+            j = j + 1
+        end
+    end
+    return new_arr
+end
+
+function reduce(fn, arr, start)
+    result = start
+    for i,v in ipairs(arr) do
+        result = fn(result, v)
+    end
+    return result
 end
